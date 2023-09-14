c8c08c16aa2bd5397432fa7d7ad80ad21404eda4
Ayush <ayshrj10@gmail.com>
2023-09-15 02:51:58 +0530

    Added things

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+function getKanyeQuote() {
+  fetch("https://api.kanye.rest/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const quote = data.quote;
+      const quoteContainer = document.createElement("div");
+      const italicQuote = document.createElement("em");
+      italicQuote.classList.add("kanye");
+      italicQuote.innerText = `"${quote}"`;
+
+      // Append the quote container below the main container
+      const container = document.querySelector("body");
+      italicQuote.appendChild(quoteContainer);
+      container.appendChild(italicQuote);
+
+      const kanye = document.createElement("div");
+      kanye.classList.add("kanye");
+      kanye.classList.add("kanye-name");
+      kanye.innerText = `-Kanye West`;
+
+      // Append the quote container below the main container
+      container.appendChild(kanye);
+    })
+    .catch((error) => {
+      console.error("Error fetching Kanye quote:", error);
+    });
+}
+
+window.onload = function () {
+  getKanyeQuote();
+};
+
 function shareLocation() {
   if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(
@@ -5,7 +38,19 @@ function shareLocation() {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
 
-        const url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+        function isMobileDevice() {
+          return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+            navigator.userAgent
+          );
+        }
+
+        let url;
+
+        if (isMobileDevice()) {
+          url = `https://maps.google.com/?q=${latitude},${longitude}`;
+        } else {
+          url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+        }
         const message = `Check out my location: ${url}`;
 
         if (navigator.share) {
@@ -13,7 +58,7 @@ function shareLocation() {
             .share({
               title: "My Location",
               text: message,
-              url: url,
+              //   url: url,
             })
             .then(() => console.log("Shared successfully"))
             .catch((error) => console.error("Error sharing:", error));
